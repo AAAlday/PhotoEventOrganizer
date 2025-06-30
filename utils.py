63c15a989f26c8eb31014f4ce8c8e32b1637903c
xGrim69 <xgrim000@gmaiil.com>
2025-06-30 16:15:07 +0800

    worker.py: Fix unsanitized event directory name disrupts folder hierarchy

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
 import sys
 from os import path
+from re import sub
 
 def getResourcePath(relativePath):
         if hasattr(sys, '_MEIPASS'):
             return path.join(sys._MEIPASS, relativePath) # _MEIPASS is hardcoded internal variable used by Pyinstaller's bootloader
         else:
             return path.join(path.abspath("."), relativePath)
+
+def sanitizeText(textToBeSanitized):
+     return sub("/", "∕", textToBeSanitized)
